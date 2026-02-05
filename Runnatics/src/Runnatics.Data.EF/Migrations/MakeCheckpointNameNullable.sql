910538937af1b03636145e1adda24f500c286c96
Manpreet Kaur <manpreetkaur.tech89@gmail.com>
2026-02-05 22:56:20 +0530

    Remove recent EF Core migrations and related SQL script
    Deleted migration files for certificate templates and fields, including audit properties and IsDefault column changes. Also removed the model snapshot and SQL script that made Checkpoint Name nullable. These changes roll back recent schema modifications and audit property additions from the database project.

--- Runnatics/src/Runnatics.Data.EF/Migrations/MakeCheckpointNameNullable.sql
+++
@@ -1,5 +0,0 @@
--- Make Checkpoint Name column nullable
--- This allows checkpoints to not have a name when a Parent Checkpoint is selected
-
-ALTER TABLE [dbo].[Checkpoints]
-ALTER COLUMN [Name] NVARCHAR(100) NULL;

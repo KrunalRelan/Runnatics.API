87ae58a79305d450fa3ba831646d03a6ca1ee1c6
KrunalRelan <kunal.r@trigunasoft.com>
2026-04-10 15:00:24 -0400

    Change Swagger UI route to /swagger instead of root
    The Swagger UI is now served at /swagger by setting the route
    prefix to "swagger" instead of an empty string. This moves the
    API documentation UI off the root URL and under /swagger.

--- Runnatics/src/Runnatics.Api/Program.cs
+++ Runnatics/src/Runnatics.Api/Program.cs
@@ -220,7 +220,7 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Runnatics API V1");
-    c.RoutePrefix = string.Empty;
+    c.RoutePrefix = "swagger";
 });
 
 if (app.Environment.IsDevelopment())
